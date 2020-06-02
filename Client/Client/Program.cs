5bbfd9fe699d246699fad84bae8869ee6a0aafcf
赵瑞斌 <hz0003234@163.com>
2020-06-02 20:01:27 +0800

    一百万次并发
    提交

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -14,50 +14,65 @@ namespace Client
         static void Main(string[] args)
         {
             Console.WriteLine("Press ESC to exit...");
+            //while (true)
+            //{//若有键按下，且是 ESC 键，则度退出循环
+            //    ConsoleKey InputKey = Console.ReadKey(true).Key;
+            //    if (InputKey == ConsoleKey.Escape) break;
+            //    if (InputKey == ConsoleKey.Spacebar)
+            //    {
+            List<Task> t = new List<Task>();
+            for (int i = 0; i < 1; i++)
+            {
+                SocketManager socketManager = new SocketManager("127.0.0.1", 11000);
+                socketManager.Connect();
+                // t.Add(Task.Factory.StartNew(() =>
+                //{
+                AddProducts(socketManager);
+                //}));
+            }
+            //Task.WaitAll(t.ToArray());
+            Console.WriteLine("客户端当前线程ID：" + Thread.CurrentThread.ManagedThreadId.ToString());
+            //}
+            //}
+        }
+        static string message = "H";
+        static void AddProducts(SocketManager socketManager)
+        {
             while (true)
             {//若有键按下，且是 ESC 键，则度退出循环
                 ConsoleKey InputKey = Console.ReadKey(true).Key;
                 if (InputKey == ConsoleKey.Escape) break;
                 if (InputKey == ConsoleKey.Spacebar)
                 {
+                    
+                    Random ran = new Random();
                     List<Task> t = new List<Task>();
-                    for (int i = 0; i < 1; i++)
+                    for (int i = 0; i < 1000000; i++)
                     {
-                        SocketManager socketManager = new SocketManager("127.0.0.1", 11000);
-                        socketManager.Connect();
-                        t.Add(Task.Factory.StartNew(() =>
+                        Person person = new Person()
                         {
-                            AddProducts(socketManager);
-                        }));
-                    }
-                    Task.WaitAll(t.ToArray());
-                    Console.WriteLine("客户端当前线程ID：" + Thread.CurrentThread.ManagedThreadId.ToString());
-                }
-            }
-        }
-        static string message = "H";
-        static void AddProducts(SocketManager socketManager)
-        {
-            Person person = new Person()
-            {
-                Aliases = string.Empty,
-                BodyLength = 10,
+                            Aliases = string.Empty,
+                            BodyLength = 10,
 
-            };
-            Random ran = new Random();
-            
-            for (int i = 0; i < 10; i++)
-            {
-                int RandKey = ran.Next(100, 1026);
-                for (int j = 0; j < RandKey; j++)
-                {
-                    person.Aliases += "1";
+                        };
+                        int RandKey = ran.Next(100, 1026);
+                        for (int j = 0; j < RandKey; j++)
+                        {
+                            person.Aliases += "1";
+                        }
+                        person.BodyLength = RandKey;
+
+                        byte[] send = System.Text.Encoding.UTF8.GetBytes(message);
+                        person.Body = ByteString.CopyFrom(send);
+                        t.Add(Task.Factory.StartNew(() =>
+                       {
+                           socketManager.Send(person);
+                       }));
+
+                        Task.WaitAll(t.ToArray());
+                        //socketManager.Send(person);
+                    }
                 }
-                person.BodyLength = RandKey;
-                
-                byte[] send = System.Text.Encoding.UTF8.GetBytes(message);
-                person.Body = ByteString.CopyFrom(send);
-                socketManager.Send(person);
             }
         }
         static void AddProducts1()
@@ -86,5 +101,6 @@ namespace Client
             Console.WriteLine("当前线程ID：" + Thread.CurrentThread.ManagedThreadId.ToString());
             Console.WriteLine("客户端结束");
         }
+
     }
 }
